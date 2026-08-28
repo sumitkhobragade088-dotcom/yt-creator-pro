4895544d3aac2b0a5284fd894aaddc109a4d7919
sumitkhobragade088-dotcom <sumitkhobragade088@gmail.com>
2026-08-28 17:56:26 +0530

    Add files via upload

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,6 +1,13 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.112.2/+esm";
 
 export const supabase = createClient(
   "https://ncxexmekzlrliicaqfcl.supabase.co",
-  "sb_publishable_mayjwNbdFk6xltqcgbfLqA_dbTjSd7q"
+  "sb_publishable_mayjwNbdFk6xltqcgbfLqA_dbTjSd7q",
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
 );
